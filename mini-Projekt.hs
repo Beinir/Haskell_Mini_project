2ad8ee82cc9dc0d198f6ab007eb54b5652c64872
Beinir Ragnuson <beinir.ragnuson@gmail.com>
2019-11-14 19:54:08 +0100

    Huffmann

--- mini-Projekt.hs
+++ mini-Projekt.hs
@@ -7,61 +7,51 @@ import Data.Function (on)
 -- map :: (a -> b) -> [a] -> [b]
 -- sortBy :: (a -> a -> Ordering) -> [a] -> [a]
 
--- Binary Tree
-data Btree = BLeaf ([Char], Int) | BBranch ([Char], Int) (Btree) (Btree) deriving Show
+-- recursive data type of a Binary Tree
+data Tree = Leaf ([Char], Int) | Node ([Char], Int) (Tree) (Tree) deriving Show
 
--- Function that returns an sorted association list with characters and their frequency 
+-- Function that returns an sorted an association list with their characters and their frequency 
 findFreq :: String -> [(Char, Int)]
 findFreq str = map (\x -> (head x, length x)) (group (sort str))
 
 -- Function that calls findFreq with a string 
 getList = findFreq "Mississippi River"
 
--- Function that a Btree from BLeafs
-mapListToTrees :: [(Char, Int)] -> [Btree]
+-- Function that a Tree from Leafs
+mapListToTrees :: [(Char, Int)] -> [Tree]
 mapListToTrees lst =  map mapToTree lst 
 
--- Function that creates a BLeaf from a tuple
-mapToTree :: (Char, Int) -> Btree
+-- Function that creates a Leaf from a tuple
+mapToTree :: (Char, Int) -> Tree
 mapToTree (c, i) =  res
-                where res = BLeaf ([c], i) 
+                where res = Leaf ([c], i) 
 
--- Pattern Matching where we are interested in comparing the second elements in out BLeafs                
-compareTrees :: Btree -> Btree -> Ordering 
-compareTrees (BLeaf   (_, a))     (BBranch (_, b) _ _) = compare a b
-compareTrees (BBranch (_, a) _ _) (BLeaf   (_, b))     = compare a b
-compareTrees (BLeaf   (_, a))     (BLeaf   (_, b))     = compare a b
-compareTrees (BBranch (_, a) _ _) (BBranch (_, b) _ _) = compare a b
+-- Pattern Matching where we are interested in comparing the second elements in out Leaves                
+compareTrees :: Tree -> Tree -> Ordering 
+compareTrees (Leaf   (_, a))     (Node (_, b) _ _) = compare a b
+compareTrees (Node (_, a) _ _) (Leaf   (_, b))     = compare a b
+compareTrees (Leaf   (_, a))     (Leaf   (_, b))     = compare a b
+compareTrees (Node (_, a) _ _) (Node (_, b) _ _) = compare a b
 
--- Function that sorts BLeafs after ascending order
-sortTreeList :: [Btree] -> [Btree]
+-- Function that sorts Leaves after ascending order
+sortTreeList :: [Tree] -> [Tree]
 sortTreeList lst = sortBy compareTrees lst
 
--- 
-getElements :: [Btree] -> [Btree]
-getElements (n1@(BBranch (v1, f1) _ _):n2@(BBranch (v2, f2) _ _):rest) = (BBranch (v1 ++ v2, f1 + f2) n1 n2) : rest
-getElements (n1@(BLeaf (v1, f1)):n2@(BLeaf (v2, f2)):rest) = (BBranch (v1 ++ v2, f1 + f2) n1 n2) : rest
-getElements (n1@(BBranch (v1, f1) _ _):n2@(BLeaf (v2, f2)):rest) = (BBranch (v1 ++ v2, f1 + f2) n1 n2) : rest
-getElements (n1@(BLeaf (v1, f1)):n2@(BBranch (v2, f2) _ _):rest) = (BBranch (v1 ++ v2, f1 + f2) n1 n2) : rest
-
-
--- Functions That builds the Tree 
-buildTree :: [Btree] -> Btree
+-- Functions That builds the Huffmann Tree 
+buildTree :: [Tree] -> Tree
 buildTree [a] = a
-buildTree lst = buildTree (getElements (sortTreeList lst))
-                                                            
-----------------------------------------------------------------------------------------
+buildTree lst = buildTree (mergeTree (sortTreeList lst))
 
--- Run
---run = sortTreeList (mapListToTrees getList)
---run = mapListToTrees getList 
---sortTreeList (mapListToTrees getList) 
+-- Mergers two leaves to one node, using pattern matching
+mergeTree :: [Tree] -> [Tree]
+mergeTree (n1@(Node (v1, f1) _ _):n2@(Node (v2, f2) _ _):rest) = (Node (v1 ++ v2, f1 + f2) n1 n2) : rest
+mergeTree (n1@(Leaf (v1, f1)):n2@(Leaf (v2, f2)):rest) = (Node (v1 ++ v2, f1 + f2) n1 n2) : rest
+mergeTree (n1@(Node (v1, f1) _ _):n2@(Leaf (v2, f2)):rest) = (Node (v1 ++ v2, f1 + f2) n1 n2) : rest
+mergeTree (n1@(Leaf (v1, f1)):n2@(Node (v2, f2) _ _):rest) = (Node (v1 ++ v2, f1 + f2) n1 n2) : rest
+                            
+-- Run huffman Codeing 
+run = buildTree (mapListToTrees getList)
 
-run1 = sortTreeList (mapListToTrees getList)
---run2 = getElements getList
-run3 = buildTree (mapListToTrees getList)
---run3 = sortTreeList (mapListToTrees getElements getList)
---run4 = getElements (mapListToTrees getList)
 
 
 
